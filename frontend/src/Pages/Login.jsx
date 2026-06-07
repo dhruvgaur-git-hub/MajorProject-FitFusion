3077c752cd0026dc02bbba192409b7f273d06ab4
Kunal2909 <kunal9425772772@gmail.com>
2026-06-08 01:21:08 +0530

    Imported frontend base from dhruv-gaur

---
+++ frontend/src/Pages/Login.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import Navbar from '../Components/Navbar';
+import {Link, useNavigate} from 'react-router-dom'
+function Login(){
+      const [email, setEmail]=useState('');
+  const [password, setPassword]= useState('');
+  const navigate= useNavigate();
+  const handleLogin = () => {
+    if(email==="dhruv@gmail.com" && password==="123"){
+      navigate('/home');
+    }   
+  };
+  return (
+    <>
+    
+    <div style={{backgroundColor: "#ffffff", minHeight: "100vh"}}>
+      <Navbar />
+    <div className="container w-50 mt-5">
+      <h2 className="mb-3">Login</h2>
+      <div className="mb-3">
+        <label htmlFor='email' className="form-label">Email</label>
+        <input type="email" className='form-control' id="email" placeholder='Enter email' onChange={e=>setEmail(e.target.value)} />
+      </div>
+      <div className="mb-3">
+        <label htmlFor='password' className='form-label'>Password</label>
+        <input type="password" className='form-control' id="password" placeholder='Enter Password' onChange={e=>setPassword(e.target.value)} />
+      </div>
+      <div className='mb-3'>
+        <button className='btn btn-success' onClick={handleLogin}>login</button>
+      </div>
+      <div className='mb-3 mt-5'>
+      <label>
+          To create a new account!
+      </label>
+      <br />
+      <Link to="/register" className="fw-semibold text-decoration-none mt-5">
+          Register
+      </Link>
+      </div>
+     </div>
+    </div>
+     
+    </>
+  )
+}
+export default Login
---
+++ frontend/src/Pages/Register.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react"
+import {Link, useNavigate } from "react-router-dom"
+function Register(){
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [phone, setPhone] = useState('')
+
+    const navigate = useNavigate()
+
+    const handleSignupClick = () => {
+    if (!name || !email || !password || !phone) {
+        alert("Please fill all fields");
+        return;
+    }
+
+    alert("Registration Successful");
+    navigate('/');
+};
+
+
+    return (
+        <div>
+            <div className="container w-50">
+                <h2 className="mb-3">Register</h2>
+                <div className="mb-3">
+                    <label for="username" className="form-label">Name</label>
+                    <input type="text" className="form-control" id="username" placeholder="Enter name" onChange={e => setName(e.target.value)} />
+                </div>
+                <div className="mb-3">
+                    <label for="email" className="form-label">Email</label>
+                    <input type="email" className="form-control" id="email" placeholder="Enter email" onChange={e => setEmail(e.target.value)} />
+                </div>
+                <div className="mb-3">
+                    <label for="password" className="form-label">Password</label>
+                    <input type="password" className="form-control" id="password" placeholder="Enter password" onChange={e => setPassword(e.target.value)} />
+                </div>
+                <div className="mb-3">
+                    <label for="phone" className="form-label">Phone</label>
+                    <input type="tel" className="form-control" id="phone" placeholder="Enter mobile no" onChange={e => setPhone(e.target.value)} />
+                </div>
+                <div className="mb-3">
+                    <label>Already have an account ?</label>
+                    <Link to='/'>Click Here To Signin</Link>
+                </div>
+                <div className="mb-3">
+                    <button className="btn btn-success" onClick={handleSignupClick}>Signup</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Register
