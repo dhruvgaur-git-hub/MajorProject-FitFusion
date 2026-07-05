5404e01a4b8bb9b4bb1814b0902bc6c0372840b7
Just-MohitChavan <mohitchavan127@gmail.com>
2026-07-05 09:06:28 +0530

    Completed SubCategory WEB APIs

---
+++ tryouts/mohit/CatalogTryout/backend/src/main/java/com/backend/entites/mongo/SubCategory.java
@@ -0,0 +1,46 @@
+package com.backend.entites.mongo;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Document(collection="subcategories")
+@CompoundIndex(
+	def = "{'categoryId':1,'name':1}",
+	unique = true
+)
+public class SubCategory {
+	
+	@Id
+	private String id;
+	
+	private String categoryId;
+	
+	private String name;
+	
+	private String description;
+	
+	private Boolean active;
+	
+	@CreatedDate
+	private LocalDateTime createdAt;
+
+	@LastModifiedDate
+	private LocalDateTime updatedAt;
+}
